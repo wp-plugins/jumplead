68c1cead5bb262142e82999705436576b15f4b7b
jumplead <jumplead@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-30 15:20:39 +0000

    tagging version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/jumplead/tags/1.3@619057 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jumplead.php
+++ jumplead.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Jumplead
-Plugin URI: http://www.jumplead.com
-Description: A plugin to add the Jumplead Lead Generation service to your blog
-Version: 1.0
+Plugin URI: http://wordpress.org/extend/plugins/jumplead/
+Description: This plugin will allow you to quickly insert the Jumplead website tracking and chat code into your website or blog.
+Version: 1.2
 Author: Jumplead
 Author URI: http://www.jumplead.com
 Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
@@ -12,7 +12,7 @@ Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
 /*  Copyright 2012  Adam Curtis  (email : adam@jumplead.com)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -45,9 +45,11 @@ function jumplead_footer() {
 <!-- Start Jumplead Code -->
 <script type="text/javascript">
     try {
+        //<![CDATA[
         window.Jumplead||function(e){function j(){return["<",f,' onload="var d=',c,";d.getElementsByTagName('head')[0].",g,"(d.",d,"('script')).",h,"='",m,"?v=",e.version,"&a=",e.account,"'\"></",f,">"].join("")}var b=document,f="body",k=b[f],d="createElement",g="appendChild",n=b[d]("div")[g](b[d]("m")),a=b[d]("iframe"),h="src",i,c="document",m=("https:"==b.location.protocol?"https://":"http://")+"www.jumplead.com/jumplead.js";a.style.display="none";a.frameBorder="0";a.id="jl-if";a.allowTransparency="true";
         n[g](a);k.insertBefore(a,k.firstChild);try{a.contentWindow[c].open()}catch(o){e.domain=b.domain,i="javascript:var d="+c+".open();d.domain='"+b.domain+"';",a[h]=i+"void(0);"}try{var l=a.contentWindow[c];l.write(j());l.close()}catch(p){a[h]=i+'d.write("'+j().replace(/"/g,'\"')+'");d.close();'}}
-        ({account:"{$tracker_id}",version:1});
+        ({account:"{$tracker_id}",version:2});
+        //]]>
     } catch(e) {}
 </script>
 <!-- End Jumplead Code -->
--- jumplead_admin.php
+++ jumplead_admin.php
@@ -5,11 +5,11 @@ if(isset($_POST['jumplead_trk_id'])) {
 	update_option('jumplead_trk_id', $jumplead_trk_id);
 	?>
 	<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div><?php
-} 
+}
 
 else {
 	//Normal page display
-	$jumplead_trk_id = get_option('jumplead_trk_id');  
+	$jumplead_trk_id = get_option('jumplead_trk_id');
 ?>
 <div class="wrap">
 	<div id="icon-options-general" class="icon32"><br></div>
@@ -26,12 +26,12 @@ else {
 					<th><label for="jumplead_trk_id">Tracker ID</label></th>
 					<td>
 						<input type="text" id="jumplead_trk_id" name="jumplead_trk_id" value="<?php echo $jumplead_trk_id ?>" size="20">
-						<span class="description">Example: JL-1303903321-1</span>
+						<span class="description">Example: JL-1111111111-1</span>
 					</td>
 				</tr>
 			</tbody>
 		</table>
-		
+
 		<p class="submit">
 			<input type="submit" name="submit" id="submit" class="button-primary" value="Save Changes">
 		</p>
